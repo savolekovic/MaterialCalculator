914275ca8a2aa4131b24af38fb095e6e515f7fe6
savolekovic <savolekovic15@gmail.com>
2024-08-29 09:43:57 +0200

    commit

--- app/src/main/java/com/example/materialcalculator/MainActivity.kt
+++ app/src/main/java/com/example/materialcalculator/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.materialcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.materialcalculator.presentation.CalculatorScreen
 import com.example.materialcalculator.ui.theme.MaterialCalculatorTheme
